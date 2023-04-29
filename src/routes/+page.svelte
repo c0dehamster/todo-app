db1949ccf57fae4014275da36815e150c297540a
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-04-29 22:09:59 +0500

    Write some basic markdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import iconSun from "../lib/images/icon-sun.svg"
+	import iconMoon from "../lib/images/icon-moon.svg"
+</script>
+
+<svelte:head>
+	<title>Frontend Mentor | Todo app</title>
+</svelte:head>
+
+<div class="app">
+	<div class="wrapper">
+		<header class="header">
+			<h1 class="heading">Todo</h1>
+
+			<!-- Do I even need an aria label for a theme switch? -->
+			<button class="button" aria-label="light">
+				<img src={iconSun} alt="" class="button__icon" />
+			</button>
+		</header>
+
+		<main class="main">
+			<p class="instruction">Drag and drop to reorder list</p>
+		</main>
+	</div>
+</div>
