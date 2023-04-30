f19c61d05df5b9ad8a5f881506254731b1a3e3fb
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-04-30 14:24:32 +0500

    Style the form

--- src/routes/Form.svelte
+++ src/routes/Form.svelte
@@ -1,12 +1,41 @@
 <script lang="ts"></script>
 
-<from class="card">
-	<button class="checkbox-circle">
+<from class="form">
+	<button class="checkbox-circle checkbox-circle--checked">
 		<span class="sr-only"> Add todo </span>
-
-		<input
-			type="text"
-			class="card__input"
-			placeholder="Create a new todo..." />
 	</button>
+
+	<input type="text" class="form__input" placeholder="Create a new todo..." />
 </from>
+
+<style>
+	.form {
+		padding-inline: 1.5rem;
+		padding-block: 1rem;
+
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+
+		border-radius: 0.25rem;
+
+		background-color: var(--color-background-card);
+	}
+
+	.form__input {
+		border: none;
+		outline: none;
+
+		background-color: transparent;
+		color: var(--color-main-contrast);
+
+		/* I did non know it is possible to style the caret at all */
+		caret-color: var(--color-primary-400);
+
+		font-size: var(--font-size-100);
+	}
+
+	.form__input::placeholder {
+		color: var(--color-main-shaded);
+	}
+</style>
