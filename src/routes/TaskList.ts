6fe0f4841ad69e9951e6e756892e33c8a0a58933
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-05-01 09:46:03 +0500

    Add the list store for tasks

---
+++ src/routes/TaskList.ts
@@ -0,0 +1,48 @@
+import { writable } from "svelte/store";
+import {v4 as uuidv4} from "uuid"
+import type { Writable } from "svelte/store";
+
+interface Item {
+    id: string
+    description: string
+    completed: boolean
+}
+
+let items: Item[] = []
+
+const createArrayStore = (items: Item[]) => {
+	const { set, update, subscribe }: Writable<{items: Item[]}> = writable({
+		items
+	})
+
+	const addItem = (description: string) => {		
+		update(store => {
+            let item = {
+                id: uuidv4(),
+                description,
+                completed: false
+            }
+
+			return {
+				...store,
+				items: [item, ...store.items],
+			}
+		})
+	}
+
+	const removeItem = (id: string) => {
+		update(store => {
+			let itemsUpdated = store.items.filter((item: Item) => item.id !== id)
+
+			return { ...store, items: itemsUpdated }
+		})
+	}
+
+	return {
+		subscribe,
+		addItem,
+		removeItem,
+	}
+}
+
+export const TasksStore = createArrayStore(items)
